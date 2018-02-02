7a94a80249fda22098860afcf07820989cf05742
zoeang <36051346+zoeang@users.noreply.github.com>
2018-02-02 14:15:10 -0600

    Cho Gain's d
    Created a function to calculate Cho's Gains d

--- AngPS2.R
+++ AngPS2.R
@@ -7,7 +7,7 @@ a<-c(548,265489,16514,651,864,31,54,231,648,23)
 b<-NULL
 xi<-NULL
 
-
+###Leemis
 leemis<-function(a){
   #argument a will be a vector of total votes
   b<-as.numeric(substr(a, start=1, stop=1)) #This will take the first signficant digit of each element of the fed vector
@@ -27,9 +27,29 @@ leemis<-function(a){
 }
 leemis(a)
 
+###Cho-Gains
+#argument a will be a vector of total votes
+ChoGains<-function(a){
+  b<-as.numeric(substr(a, start=1, stop=1)) #This will take the first signficant digit of each element of the fed vector
+  proportions<-NULL
+  for (i in 1:9){ #This loop creates x_i, a proportion of occurrence of each significant digit
+    y<-sum(b==i)/length(a)
+    proportions<-c(proportions,y)
+  }
+  xi<-proportions
+  elemsum<-NULL
+  for (i in 1:9){ #This loop will calculate ChoGains' d
+    element<-((xi[i]-log((1+1/i), base=10))^2)
+    elemsum<-c(elemsum,element)
+    elemsumvec<-sum(elemsum)
+    d<-sqrt(elemsumvec)
+  }
+return(d)
+}
 
+ChoGains(a)
 
-
+#####
 if(leemis()>=1.212){
   print("Significant when $\alpha=0.01")
   if
